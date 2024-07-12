9ca6312a262940e694c62aefc21c7f443fdac064
101082 <101082@MACTECH.LOC>
2024-07-12 15:26:25 +0530

    Initial Commit

---
+++ telegram_screen/lib/calls_list.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class CallList extends StatefulWidget {
+  const CallList({super.key});
+
+  @override
+  State<CallList> createState() => _CallListState();
+}
+
+class _CallListState extends State<CallList> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+    ); 
+  }
+}
---
+++ telegram_screen/lib/info.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class InfoDetails extends StatefulWidget {
+  const InfoDetails({super.key});
+
+  @override
+  State<InfoDetails> createState() => _InfoDetailsState();
+}
+
+class _InfoDetailsState extends State<InfoDetails> {
+
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+    );
+  }
+}
